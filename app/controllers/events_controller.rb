3499b1f5e6b629244033b61831c4409dd27ef976
loqman <loqmanhakimi@gmail.com>
2016-12-08 11:31:58 +0330

    Add events actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,47 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+    @event = current_user.events.new
+    render layout: false
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  def share
+
+  end
+
+  private
+  def event_params
+    params.require('event').permit!
+  end
+
+  def set_event
+    @event = Event.find params[:id]
+  end
 
 end
